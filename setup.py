1d38fd13b9c9de9f5f2b705c3f1eedf4a4c0ef3c
CyanBook <lorenzo.delmonte05@gmail.com>
2020-07-11 12:45:42 +0000

    Version updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="deezepy",
-    version="0.2",
+    version="0.2.1",
     author="CyanBook",
     author_email="lorenzo.delmonte05@gmail.com",
     description="Deezer wrapper written in Python",
